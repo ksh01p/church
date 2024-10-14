9e8ba682edf38af06d54bded4a70d9b376e167c8
ksh01p <william7872ksh@handong.ac.kr>
2024-10-14 15:12:03 +0900

    default page 생성

---
+++ src/main/java/com/example/church_web/ChurchWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.church_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/church_web/ChurchWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.church_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
