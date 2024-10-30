2c69365a95d5bb20017141f19b775f94dd0d26d0
ksh01p <william7872ksh@handong.ac.kr>
2024-10-30 19:39:53 +0900

    admin 페이지 구현 완료
    admin 페이지 구현 완료

--- src/main/java/com/example/church_web/controller/page/AdminController.java
+++ src/main/java/com/example/church_web/controller/page/AdminController.java
@@ -1,26 +1,61 @@
 package com.example.church_web.controller.page;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("")
+
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
-    //    @GetMapping({"","/"}) //이 안에 있는 주소값은 꼭 유니크해야함!!
-//    public String empty(){
-//        return "redirect:/index";
-//    }
-    @GetMapping({"/admin"}) //이 안에 있는 주소값은 꼭 유니크해야함!!
-    public String admin(){
-        return "admin";
+
+    @GetMapping
+    public String adminHome() {
+        return "admin/home"; // admin/index.html 파일이 있어야 합니다.
+    }
+
+    // 환영합니다 관리 페이지 매핑
+    @GetMapping("/welcome/introduce")
+    public String manageIntroduce() {
+        return "admin/welcome/introduce";
+    }
+
+    @GetMapping("/welcome/fam_signup")
+    public String manageFamSignup() {
+        return "admin/welcome/fam_signup";
+    }
+
+    // 소개합니다 관리 페이지 매핑
+    @GetMapping("/introduction/pastor")
+    public String managePastor() {
+        return "admin/introduction/pastor";
+    }
+
+    @GetMapping("/introduction/church")
+    public String manageChurch() {
+        return "admin/introduction/church";
+    }
+
+    // 예배합니다 관리 페이지 매핑
+    @GetMapping("/worship/videos")
+    public String manageVideos() {
+        return "admin/worship/videos";
+    }
+
+    @GetMapping("/worship/sunday")
+    public String manageSunday() {
+        return "admin/worship/sunday";
+    }
+
+    // 양육 신청 관리 페이지 매핑
+    @GetMapping("/application/apply")
+    public String manageApplication() {
+        return "admin/application/apply";
+    }
+
+    // 선교 상황 관리 페이지 매핑
+    @GetMapping("/mission/country")
+    public String manageMissionCountry() {
+        return "admin/mission/country";
     }
 }
-
-/*
-@GetMapping("/{page"}")
-public String page(@PathVariable String page){return "welcome/"+page;}
-
---> path variabl e을 선언해서 url 에 있는 값을 변수로 받아 올 수 있는 기능 !
-*/
-
