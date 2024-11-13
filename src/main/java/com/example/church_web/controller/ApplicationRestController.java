8f2cb6f861e7f1a92612171751bec248692bdb8d
ksh01p <william7872ksh@handong.ac.kr>
2024-11-13 15:22:23 +0900

    Miner Commit
    Miner Commit

--- src/main/java/com/example/church_web/controller/ApplicationRestController.java
+++ src/main/java/com/example/church_web/controller/ApplicationRestController.java
@@ -22,7 +22,6 @@ public class ApplicationRestController {
         return applicationService.createApplication(params);
     }
 
-
 }
 
 
--- src/main/java/com/example/church_web/controller/page/ApplicationController.java
+++ src/main/java/com/example/church_web/controller/page/ApplicationController.java
@@ -20,6 +20,9 @@ public class ApplicationController {
         return "application/" + page;
     }
 
+
+
+
 }
 
 /*
